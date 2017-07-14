9d7f95afe3ccdf1e89c964ba66701cd886856fc5
yewenhe0904 <yewenhe0904@gmail.com>
2017-07-14 12:17:44 -0500

    add webdriver, can retry, can subbmit game score

---
+++ test.py
@@ -0,0 +1,17 @@
+import sys,os
+# print(sys.path)
+print(os.getcwd())
+sys.path.append(os.path.join(os.getcwd(),'geckodriver-dir/geckodriver'))
+print(sys.path)
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Firefox()
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element_by_name("q")
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+driver.close()
